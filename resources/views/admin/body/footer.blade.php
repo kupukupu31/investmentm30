03df415deb879f22d1390f04f45b8d51b895d4f9
Alden Vidal <aldenvidal25@gmail.com>
2023-02-21 12:51:23 +0800

    segmented

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -1,14 +1,14 @@
 <footer class="footer">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <script>document.write(new Date().getFullYear())</script> tiktokerist.
-                            </div>
-                            <div class="col-sm-6">
-                                <div class="text-sm-end d-none d-sm-block">
-                                    Crafted with <i class="mdi mdi-heart text-danger"></i> tiktok
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                2023 © M30 Investments
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Crafted with <i class="mdi mdi-heart text-danger"></i> by M30 Investments
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
